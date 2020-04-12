99b150f5f4d0d81eccdd697c6032bd51fa5e3d96
Tim St.Clair <tim.stclair@gmail.com>
2020-04-12 21:34:50 +1000

    added the coursesuite bar

--- public/routes/_header.inc.php
+++ public/routes/_header.inc.php
@@ -26,7 +26,19 @@
 		<?php include "_analytics.inc.php"; ?>
     </head>
     <body class="<?php echo str_replace('.inc.php','',$fn); ?> uk-position-relative">
-    	<header class="uk-position-top-right uk-position-small uk-position-fixed">
+    	<div uk-sticky="media: 960" class="uk-navbar-container my-bg-blue uk-sticky uk-sticky-fixed uk-navbar-transparent">
+			<div class="uk-container uk-container-expand coursesuite-bar">
+				<div class=" uk-padding-small">
+					<img src="assets/coursesuite-glyph.svg" style="width:1rem" class="embiggen">
+					<a href="https://www.coursesuite.com/?from=presentation2scorm" target="_self">Coursesuite</a>
+					<a href="https://www.courseassembler.com/?from=presentation2scorm" target="_self">Course Assembler</a>
+					<a href="https://video.to-scorm.com/?from=presentation2scorm" target="_self">Video 2 Scorm</a>
+					<a href="https://presentation.to-scorm.com/?from=presentation2scorm" target="_self">Audio Presentation Ninja</a>
+					<a href="https://coursebuildr.coursesuite.ninja/app/login/?from=presentation2scorm" target="_self">CourseBuilder</a>
+				</div>
+			</div>
+		</div>
+    	<header class="uk-position-top-right uk-position-large uk-position-fixed">
 			<ul class="uk-navbar-nav">
 				<li><a href="/">Home</a></li>
 				<li><a href="/faq">Faq</a></li>
@@ -34,4 +46,4 @@
 				<li><?php include "_launch.inc.php"; ?></li>
 			</ul>
 		</header>
-		<img src="assets/logo.svg" class="uk-position-top-left uk-position-small ap-logo" width="500">
+		<img src="assets/logo.svg" class="uk-position-top-left uk-position-small ap-logo" width="500" style="top:2rem">
